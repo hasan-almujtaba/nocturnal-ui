91128fef7909810794fb582d31f81a12f55af6e5
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-12-10 19:31:17 +0700

    feat: add resolver for import alias

--- core/vite.config.ts
+++ core/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import path, { resolve } from 'path'
 import dts from '../plugins/vite-plugin-dts-generator'
 
 export default defineConfig({
 	plugins: [vue(), dts()],
+	resolve: {
+		alias: {
+			'~/': `${path.resolve(__dirname, 'src')}/`,
+		},
+	},
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/main.ts'),
