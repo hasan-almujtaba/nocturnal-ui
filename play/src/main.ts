a4b3c60f7f0232db8ab9146f3add04705f558fd0
Hasan Al Mujtaba <hasan.almujtaba01@Gmail.com>
2022-12-18 02:38:01 +0700

    chore: remove playground

--- play/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import NocturnalUI from 'nocturnal-ui'
-import './style.css'
-
-const app = createApp(App)
-
-// app.use(NocturnalUI)
-app.mount('#app')
